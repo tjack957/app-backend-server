8280edcce58286b75e4fd653317969c456bd5502
mikeflyuwt <56419237+mikeflyuwt@users.noreply.github.com>
2020-05-16 19:26:15 -0700

    Update weather.js
    reverted hourly back to hardcoded lon/lat

--- routes/weather.js
+++ routes/weather.js
@@ -1,5 +1,4 @@
 const API_KEY = process.env.OPEN_WEATHER_KEY
-const API_KEY2 = process.env.WEATHER_BIT_KEY
 const ZIP_CODE = 98402
 const LAT = 47.2451
 const LON = -122.4380
@@ -96,48 +95,48 @@ router.get("/current", (req, res) => {
 
 })
 
-/**
- * @api {post} /weather/hourly Request hourly forecasted weather from OpenWeatherMap.org
- * @apiName Weather API
- * @apiGroup OpenWeatherMap
- * 
- * @apiHeader {String} authorization JWT provided from Auth post
- * 
- * @apiDescription This end point is a pass through to the OpenWeatherMap API. 
- */ 
-router.post("/hourly", (req, res) => {
+// /**
+//  * @api {post} /weather/hourly Request hourly forecasted weather from OpenWeatherMap.org
+//  * @apiName Weather API
+//  * @apiGroup OpenWeatherMap
+//  * 
+//  * @apiHeader {String} authorization JWT provided from Auth post
+//  * 
+//  * @apiDescription This end point is a pass through to the OpenWeatherMap API. 
+//  */ 
+// router.post("/hourly", (req, res) => {
 
-    console.log(req.decoded)
-    let query = req.query.search;
+//     console.log(req.decoded)
+//     let query = req.query.search;
     
-    //let url = `https://pro.openweathermap.org/data/2.5/forecast/hourly?zip=` + query + `${COUNTRY_CODE}&appid=${API_KEY}`    
-    let url = `https://api.weatherbit.io/v2.0/forecast/hourly?postal_code=` + query + `&country=US&key=${API_KEY2}&hours=24`
+//     //let url = `https://pro.openweathermap.org/data/2.5/forecast/hourly?zip=` + query + `${COUNTRY_CODE}&appid=${API_KEY}`    
+//     let url = `https://api.weatherbit.io/v2.0/forecast/hourly?postal_code=` + query + `&country=US&key=${API_KEY2}&hours=24`
     
-    //find the query string (parameters) sent to this end point and pass them on to
-    // openweathermap api call 
-    let n = req.originalUrl.indexOf('?') + 1
-    if(n > 0) {
-        url += '&' + req.originalUrl.substring(n)
-    }
+//     //find the query string (parameters) sent to this end point and pass them on to
+//     // openweathermap api call 
+//     let n = req.originalUrl.indexOf('?') + 1
+//     if(n > 0) {
+//         url += '&' + req.originalUrl.substring(n)
+//     }
 
-    //When this web service gets a request, make a request to the OpenWeatherMap Web service
-    request(url, function (error, response, body) {
-        if (error) {
-            res.send(error)
-        } else {
-            // pass on everything (try out each of these in Postman to see the difference)
-            // res.send(response);
+//     //When this web service gets a request, make a request to the OpenWeatherMap Web service
+//     request(url, function (error, response, body) {
+//         if (error) {
+//             res.send(error)
+//         } else {
+//             // pass on everything (try out each of these in Postman to see the difference)
+//             // res.send(response);
             
-            // or just pass on the body
+//             // or just pass on the body
 
-            var n = body.indexOf("{")
-            var nakidBody = body.substring(n - 1)
+//             var n = body.indexOf("{")
+//             var nakidBody = body.substring(n - 1)
 
-            res.send(nakidBody)
-        }
-    })
+//             res.send(nakidBody)
+//         }
+//     })
 
-})
+// })
 
 /**
  * @api {get} /weather/hourly Request hourly forecasted weather from OpenWeatherMap.org
