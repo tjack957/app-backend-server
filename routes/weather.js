03cdecbe388bbcd4ecc5951c553e520fe9e91fdb
mikeflyuwt <56419237+mikeflyuwt@users.noreply.github.com>
2020-05-25 12:59:24 -0700

    Update weather.js
    added weatherbit daily endppoint

--- routes/weather.js
+++ routes/weather.js
@@ -141,13 +141,13 @@ router.get("/current", (req, res) => {
 
 
 /**
- * @api {get} /weather/hourly Request hourly forecasted weather from OpenWeatherMap.org
+ * @api {get} /weather/hourly Request hourly forecasted weather from Weatherbit.io
  * @apiName Weather API
- * @apiGroup OpenWeatherMap
+ * @apiGroup Weatherbit
  * 
  * @apiHeader {String} authorization JWT provided from Auth get
  * 
- * @apiDescription This end point is a pass through to the OpenWeatherMap API. 
+ * @apiDescription This end point is a pass through to the Weatherbit API. 
  */ 
 router.get("/hourly", (req, res) => {
 
@@ -155,9 +155,46 @@ router.get("/hourly", (req, res) => {
     
     let url = `https://api.weatherbit.io/v2.0/forecast/hourly?key=${API_KEY2}`
     //let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${LAT}&lon=${LON}&exclude=${EXCLUDE}&appid=${API_KEY}`
+     
+    let n = req.originalUrl.indexOf('?') + 1
+    if(n > 0) {
+        url += '&' + req.originalUrl.substring(n)
+    }
+
+    //When this web service gets a request, make a request to the OpenWeatherMap Web service
+    request(url, function (error, response, body) {
+        if (error) {
+            res.send(error)
+        } else {
+            // pass on everything (try out each of these in Postman to see the difference)
+            // res.send(response);
+            
+            // or just pass on the body
+
+            var n = body.indexOf("{")
+            var nakidBody = body.substring(n - 1)
+
+            res.send(nakidBody)
+        }
+    })
+
+})
+
+/**
+ * @api {get} /weather/daily Request daily forecasted weather from Weatherbit.io
+ * @apiName Weather API
+ * @apiGroup Weatherbit
+ * 
+ * @apiHeader {String} authorization JWT provided from Auth get
+ * 
+ * @apiDescription This end point is a pass through to the Weatherbit API. 
+ */ 
+router.get("/daily", (req, res) => {
+
+    console.log(req.decoded)
     
-    //find the query string (parameters) sent to this end point and pass them on to
-    // openweathermap api call 
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${API_KEY2}`
+ 
     let n = req.originalUrl.indexOf('?') + 1
     if(n > 0) {
         url += '&' + req.originalUrl.substring(n)
